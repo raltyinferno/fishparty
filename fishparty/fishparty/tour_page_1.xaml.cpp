88efe2793b066e19cf168a07e7100a452a4d21b0
tjcraddick <tjcraddick@alaska.edu>
2018-02-18 14:57:59 -0900

    Here is an example of the basic timeout functionality. Adjust "time_seconds" value to change the period from the current 5 minutes. I'll keep working at it to learn more, but it looks like this will need to be implemted for each page instead of within a separate global class because the ThreadPoolTimer functionality requires frame reference for UI.

--- fishparty/fishparty/tour_page_1.xaml.cpp
+++ fishparty/fishparty/tour_page_1.xaml.cpp
@@ -19,15 +19,96 @@ using namespace Windows::UI::Xaml::Input;
 using namespace Windows::UI::Xaml::Media;
 using namespace Windows::UI::Xaml::Navigation;
 
+
+using namespace Windows::System::Threading;
+using namespace Windows::UI::Core;
+
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+ThreadPoolTimer ^ home_timer;
 
 tour_page_1::tour_page_1()
 {
 	InitializeComponent();
 }
 
+void fishparty::tour_page_1::start_Timer()
+{
+	TimeSpan period;
+	int time_seconds = 60 * 5; // Pull from file for adjustable timeout
+
+	period.Duration = time_seconds * 10000000; // 10,000,000 ticks per second
+
+
+	canceled = false;
+
+
+	home_timer = ThreadPoolTimer::CreateTimer(ref new TimerElapsedHandler([&](ThreadPoolTimer^ source)
+	{
+		//
+		// TODO: Work
+		//
+		//this->Frame->Navigate(Windows::UI::Xaml::Interop::TypeName(MainPage::typeid));
+
+		//
+		// Update the UI thread by using the UI core dispatcher.
+		//
+		Dispatcher->RunAsync(CoreDispatcherPriority::High,
+			ref new DispatchedHandler([&]()
+		{
+			//
+			// UI components can be accessed within this scope.
+			//
+
+		}));
+
+		
+
+	}), period, ref new TimerDestroyedHandler([&](ThreadPoolTimer ^ source)
+	{
+		//
+		// TODO: Handle work cancellation/completion.
+		//
+
+		Dispatcher->RunAsync(CoreDispatcherPriority::High,
+			ref new DispatchedHandler([&]()
+		{
+			//
+			// Update the UI thread by using the UI core dispatcher.
+			//
+
+			if (!canceled)
+			{
+				// Timer completed.
+
+				this->Frame->Navigate(Windows::UI::Xaml::Interop::TypeName(MainPage::typeid));
+			}
+			else
+			{
+				// Timer cancelled.
+				start_Timer();
+			}
+
+		}));
+	}));
+}
+
 
 void fishparty::tour_page_1::home_button_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
 {
+	canceled = true;
+	home_timer->Cancel();
 	this->Frame->Navigate(Windows::UI::Xaml::Interop::TypeName(MainPage::typeid));
+}
+
+
+void fishparty::tour_page_1::Page_Loaded(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	start_Timer();
+}
+
+
+void fishparty::tour_page_1::timer_button_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	canceled = true;
+	home_timer->Cancel();
 }
