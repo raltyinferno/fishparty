e6e9f75b2237b975ac3df4cbbdc6dee759d162ba
tjcraddick <tjcraddick@alaska.edu>
2018-02-21 20:08:44 -0900

    Initial attempt at the animated home page consisting of four bubbles of varying sizes and speeds. I haven't figured out how to properly code an event handler for when the animation finishes, when I do I want to randomize the bubble starting location. I also want to figure out how to utilize variables in the XAML file so that I can have some oscillation in the motion if possible.

--- fishparty/fishparty/MainPage.xaml.h
+++ fishparty/fishparty/MainPage.xaml.h
@@ -13,15 +13,23 @@ namespace fishparty
 	/// <summary>
 	/// An empty page that can be used on its own or navigated to within a Frame.
 	/// </summary>
+	//ref class MainPage;
+	//public delegate void AnimationEventHandler(MainPage^ sender, Platform::String^ msg);
+
 	public ref class MainPage sealed
 	{
 	public:
+		//event AnimationEventHandler^ SomethingHappened;
 		MainPage();
 
 	private:
-		void inputButton_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void animate_Bubble();
+		void anim_Completed(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
 
 		void start_tour_button_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
 		void explore_button_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void Page_Loaded(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
 	};
 }
+
+
