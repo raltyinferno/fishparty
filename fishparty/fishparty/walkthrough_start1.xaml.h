385890aa0e4cd8ddbc827906946956ae202d91e6
Kai Davids Schell <raltyinferno@gmail.com>
2018-03-19 18:01:31 -0800

    added walkthrough page 1 with template

---
+++ fishparty/fishparty/walkthrough_start1.xaml.h
@@ -0,0 +1,21 @@
+﻿//
+// walkthrough_start1.xaml.h
+// Declaration of the walkthrough_start1 class
+//
+
+#pragma once
+
+#include "walkthrough_start1.g.h"
+
+namespace fishparty
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	[Windows::Foundation::Metadata::WebHostHidden]
+	public ref class walkthrough_start1 sealed
+	{
+	public:
+		walkthrough_start1();
+	};
+}
