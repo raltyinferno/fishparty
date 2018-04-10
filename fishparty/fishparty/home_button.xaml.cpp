852a5b4673eb8be79de340fe8b069410ae488f62
Kai Davids Schell <raltyinferno@gmail.com>
2018-04-09 18:05:27 -0800

    added home button user control, example of how to include on page is in water_treatment.xaml

---
+++ fishparty/fishparty/home_button.xaml.cpp
@@ -0,0 +1,35 @@
+﻿//
+// home_button.xaml.cpp
+// Implementation of the home_button class
+//
+
+#include "pch.h"
+#include "home_button.xaml.h"
+
+using namespace fishparty;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+home_button::home_button()
+{
+	InitializeComponent();
+}
+
+
+void fishparty::home_button::navigate_home(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	auto rootFrame = dynamic_cast<Frame^>(Window::Current->Content);
+
+	rootFrame->Navigate(Windows::UI::Xaml::Interop::TypeName(MainPage::typeid));
+}
---
+++ fishparty/fishparty/home_button.xaml.h
@@ -0,0 +1,20 @@
+﻿//
+// home_button.xaml.h
+// Declaration of the home_button class
+//
+
+#pragma once
+
+#include "home_button.g.h"
+
+namespace fishparty
+{
+	[Windows::Foundation::Metadata::WebHostHidden]
+	public ref class home_button sealed
+	{
+	public:
+		home_button();
+	private:
+		void navigate_home(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+	};
+}
