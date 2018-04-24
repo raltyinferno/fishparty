c4263053e849dc44ccdd9ee701a78ed0ab05f237
Kai Davids Schell <raltyinferno@gmail.com>
2018-04-23 17:11:48 -0800

    added new control to walkthrough pages up through F4

---
+++ fishparty/fishparty/walkthrough_home_nav.xaml.h
@@ -0,0 +1,18 @@
+﻿//
+// walkthrough_home_nav.xaml.h
+// Declaration of the walkthrough_home_nav class
+//
+
+#pragma once
+
+#include "walkthrough_home_nav.g.h"
+
+namespace fishparty
+{
+	[Windows::Foundation::Metadata::WebHostHidden]
+	public ref class walkthrough_home_nav sealed
+	{
+	public:
+		walkthrough_home_nav();
+	};
+}
