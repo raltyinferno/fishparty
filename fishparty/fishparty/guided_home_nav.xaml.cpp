77503d057063634f4c7743dbbb84ff18790deb3e
Kai Davids Schell <raltyinferno@gmail.com>
2018-05-17 17:35:47 -0800

    changed nav buttons on guided tour to line up with those on the walkthrough for consistency

---
+++ fishparty/fishparty/guided_home_nav.xaml.cpp
@@ -0,0 +1,27 @@
+﻿//
+// guided_home_nav.xaml.cpp
+// Implementation of the guided_home_nav class
+//
+
+#include "pch.h"
+#include "guided_home_nav.xaml.h"
+
+using namespace fishparty;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+guided_home_nav::guided_home_nav()
+{
+	InitializeComponent();
+}
---
+++ fishparty/fishparty/guided_home_nav.xaml.h
@@ -0,0 +1,18 @@
+﻿//
+// guided_home_nav.xaml.h
+// Declaration of the guided_home_nav class
+//
+
+#pragma once
+
+#include "guided_home_nav.g.h"
+
+namespace fishparty
+{
+	[Windows::Foundation::Metadata::WebHostHidden]
+	public ref class guided_home_nav sealed
+	{
+	public:
+		guided_home_nav();
+	};
+}
